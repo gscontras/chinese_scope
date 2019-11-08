514096c20a107aca5f97195b841bc574177f3473
Greg Scontras <gscontras@gmail.com>
2019-11-08 11:54:34 -0800

    posted expt, analysis script

--- experiments/10-english-goal-manipulation/Results/analysis.R
+++ experiments/10-english-goal-manipulation/Results/analysis.R
@@ -3,12 +3,12 @@ library(reshape2)
 library(lme4)
 library(dplyr)
 
-setwd("~/git/chinese_scope/experiments/9-english-QUD-revised/Submiterator-master/")
+setwd("~/git/chinese_scope/experiments/10-english-goal-manipulation/Submiterator-master/")
 
 source("../results/helpers.r")
 
-d = read.csv("9-expanded-english-trials.csv",header=T)
-s = read.csv("9-expanded-english-subject_information.csv",header=T)
+d = read.csv("10-english-goal-manipulation-trials.csv",header=T)
+s = read.csv("10-english-goal-manipulation-subject_information.csv",header=T)
 
 d$language = s$language[match(d$workerid,s$workerid)]
 #d$describe = s$describe[match(d$workerid,s$workerid)]
@@ -25,19 +25,27 @@ d$assess = s$assess[match(d$workerid,s$workerid)]
 unique(d$language)
 
 # only English as native language
-d = d[d$language!=""&d$language!="Pakistan"&d$language!="United States",]
+d = d[d$language=="English"|
+        d$language=="english"|
+        d$language=="ENGLISH"|
+        d$language=="eng"|
+        d$language=="Engish"|
+        d$language=="English "|
+        d$language=="Enligsh"|
+        d$language=="AENGLISH",]
 #d = d[d$assess=="Yes",]
+unique(d$language)
 
-length(unique(d$workerid)) # n=94 (100)
+length(unique(d$workerid)) # n=377 (XXX)
 
 ################################
 
 t = d[d$trial_type=="one_slider"&d$item!="control1"&d$item!="control2"&d$item!="control3"&d$item!="control4"&d$item!="control5"&d$item!="control6"&d$item!="control7"&d$item!="control8",]
 
-e_agg = aggregate(response~context*number*QUD*quantifier,data=t,FUN=mean)
-e_agg
+#e_agg = aggregate(response~context*number*QUD*quantifier,data=t,FUN=mean)
+#e_agg
 
-table(t$context,t$number,t$QUD,t$quantifier)
+#table(t$context,t$number,t$QUD,t$quantifier)
 
 
 e_numeral_s = bootsSummary(data=t[t$quantifier=="numeral",], measurevar="response", groupvars=c("context","number","QUD"))
@@ -46,11 +54,27 @@ e_numeral_plot = ggplot(data=e_numeral_s,aes(x=number,y=response,fill=context))+
   geom_bar(stat="identity",color="black",position=position_dodge())+
   geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=number, width=0.1),position=position_dodge(0.9))+
   ylim(0,1)+
-  #labs("order\npreference")+
+  ylab("endorsement rate") +
+  labs(fill="early-success")+
   facet_wrap(~QUD)+
   theme_bw()#+
-#ggsave("../results/english-numeral.png")
 e_numeral_plot
+#ggsave("../results/english-numeral.png")
+
+
+e_quantifier_s = bootsSummary(data=t[t$quantifier=="every",], measurevar="response", groupvars=c("context","QUD"))
+
+e_quantifier_plot = ggplot(data=e_quantifier_s,aes(x=QUD,y=response,fill=context))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=QUD, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  ylab("endorsement rate") +
+  labs(fill="early-success")+
+  #facet_wrap(~QUD)+
+  theme_bw()#+
+e_quantifier_plot
+#ggsave("../results/english-quantifier.png")
+
 
 context_s = bootsSummary(data=t[t$quantifier=="numeral",], measurevar="response", groupvars=c("context","number"))
 
@@ -89,4 +113,21 @@ e_QUDi_plot = ggplot(data=e_QUDi_s,aes(x=number,y=response,fill=context))+
 e_QUDi_plot
 
 
-summary(lmer(response~context:number+number+QUD:number+(1|workerid)+(1|item),data=t))
+#number
+summary(glm(response~context*QUD*number,data=t[t$quantifier=="numeral",]))
+
+#every
+summary(glm(response~context*QUD,data=t[t$quantifier=="every",]))
+
+
+e_combined_s = bootsSummary(data=t, measurevar="response", groupvars=c("context","number","QUD","quantifier"))
+
+e_combined_plot = ggplot(data=e_combined_s,aes(x=number,y=response,fill=context))+
+  geom_bar(stat="identity",color="black",position=position_dodge())+
+  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=number, width=0.1),position=position_dodge(0.9))+
+  ylim(0,1)+
+  #labs("order\npreference")+
+  facet_wrap(quantifier~QUD)+
+  theme_bw()#+
+e_combined_plot
+#ggsave("../results/english-combined.png")
