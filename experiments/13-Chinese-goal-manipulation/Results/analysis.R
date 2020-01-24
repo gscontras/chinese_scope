230fafbbb29d157a067b51777dcc2fa551b715ff
Greg Scontras <gscontras@gmail.com>
2020-01-24 10:10:19 -0800

    additional results

--- experiments/13-Chinese-goal-manipulation/Results/analysis.R
+++ experiments/13-Chinese-goal-manipulation/Results/analysis.R
@@ -3,76 +3,50 @@ library(reshape2)
 library(lme4)
 library(dplyr)
 
-setwd("~/git/chinese_scope/experiments/10-english-goal-manipulation/Submiterator-master/")
+setwd("~/git/chinese_scope/experiments/13-chinese-goal-manipulation/results/")
 
-source("../results/helpers.r")
+source("helpers.r")
 
-d = read.csv("10-english-goal-manipulation-trials.csv",header=T)
-s = read.csv("10-english-goal-manipulation-subject_information.csv",header=T)
+d = read.csv("results.csv",header=T)
 
-d$language = s$language[match(d$workerid,s$workerid)]
-#d$describe = s$describe[match(d$workerid,s$workerid)]
-#d$lived = s$lived[match(d$workerid,s$workerid)]
-d$age = s$age[match(d$workerid,s$workerid)]
-d$gender = s$gender[match(d$workerid,s$workerid)]
-#d$proficiency = s$proficiency[match(d$workerid,s$workerid)]
-#d$outsideLanguage = s$outsideLanguage[match(d$workerid,s$workerid)]
-#d$homeLanguage = s$homeLanguage[match(d$workerid,s$workerid)]
-#d$yearsLived = s$yearsLived[match(d$workerid,s$workerid)]
-d$comments = s$comments[match(d$workerid,s$workerid)]
-d$assess = s$assess[match(d$workerid,s$workerid)]
 
 unique(d$language)
 
-# only English as native language
-d = d[d$language=="English"|
-        d$language=="english"|
-        d$language=="ENGLISH"|
-        d$language=="eng"|
-        d$language=="Engish"|
-        d$language=="English "|
-        d$language=="Enligsh"|
-        d$language=="AENGLISH"|
-        d$language=="Englist"|
-        d$language=="english"|
-        d$language=="Wnglish",]
-#d = d[d$assess=="Yes",]
+# only lived both before and after 8 in Chinese-speaking country
+d = d[d$lived=="both8",]
+# only 5plus years in Chinese-speaking country
+d = d[d$yearsLived=="5plus",]
+# only Mandarin as native language
+d = d[d$language!="粤语"&
+        d$language!="United States"&
+        d$language!=""&
+        d$language!="CANTONESE"&
+        d$language!="ENGLISH"&
+        d$language!="2"&
+        d$language!="你阅读了操作说明"&
+        d$language!="潮州"&
+        d$language!="中国"&
+        d$language!="廣東話"&
+        d$language!="Cantonese",]
 unique(d$language)
 
-length(unique(d$workerid)) # n=546 (600)
+length(unique(d$participant_id)) # n=60 (105)
 
 ################################
 
-t = d[d$trial_type=="one_slider"&d$item!="control1"&d$item!="control2"&d$item!="control3"&d$item!="control4"&d$item!="control5"&d$item!="control6"&d$item!="control7"&d$item!="control8",]
-
-#e_agg = aggregate(response~context*number*QUD*quantifier,data=t,FUN=mean)
-#e_agg
-
-#table(t$context,t$number,t$QUD,t$quantifier)
 
 
-e_numeral_s = bootsSummary(data=t[t$quantifier=="numeral",], measurevar="response", groupvars=c("context","number","QUD"))
-
-e_numeral_plot = ggplot(data=e_numeral_s,aes(x=number,y=response,fill=context))+
-  geom_bar(stat="identity",color="black",position=position_dodge())+
-  geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=number, width=0.1),position=position_dodge(0.9))+
-  ylim(0,1)+
-  ylab("endorsement rate") +
-  labs(fill="early-success")+
-  facet_wrap(~QUD)+
-  theme_bw()#+
-e_numeral_plot
-#ggsave("../results/english-numeral.png")
 
 
-e_quantifier_s = bootsSummary(data=t[t$quantifier=="every",], measurevar="response", groupvars=c("context","QUD"))
 
-e_quantifier_plot = ggplot(data=e_quantifier_s,aes(x=QUD,y=response,fill=context))+
+e_quantifier_s = bootsSummary(data=d, measurevar="response", groupvars=c("QUD"))
+
+e_quantifier_plot = ggplot(data=e_quantifier_s,aes(x=QUD,y=response))+
   geom_bar(stat="identity",color="black",position=position_dodge())+
   geom_errorbar(aes(ymin=bootsci_low, ymax=bootsci_high, x=QUD, width=0.1),position=position_dodge(0.9))+
   ylim(0,1)+
   ylab("endorsement rate") +
-  labs(fill="early-success")+
+  #labs(fill="early-success")+
   #facet_wrap(~QUD)+
   theme_bw()#+
 e_quantifier_plot
